8aa584979c701e2d16a7d19f131d198c2de38409
marsmallowz <alsandymaulana15@email.com>
2023-07-19 12:21:53 +0700

    update navbar show current page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <div className="flex gap-3 py-10">
-      <Link href={"/"}>Home</Link>
-      <Link href={"/projects"}>Project</Link>
-      <div className="cursor-not-allowed">Blog</div>
+    <div className="flex gap-3 py-10 px-2 font-medium">
+      <Link
+        href={"/"}
+        className={pathname !== "/" ? "text-gray-500" : "text-black"}
+      >
+        Home
+      </Link>
+      <Link
+        href={"/projects"}
+        className={pathname !== "/projects" ? "text-gray-500" : "text-black"}
+      >
+        Project
+      </Link>
+      <div className="text-gray-500 cursor-not-allowed ">Blog</div>
     </div>
   );
 }
