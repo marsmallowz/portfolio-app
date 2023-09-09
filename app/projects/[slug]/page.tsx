654ea639d9eeb451b7032c06c8393df7c30b780c
marsmallowz <alsandymaulana15@email.com>
2023-09-09 13:03:52 +0700

    Fix path to access image showcase

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -6,7 +6,6 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import CustomLink from "@/components/mdx/CustomLink";
 import CustomImg from "@/components/mdx/CustomImg";
 import { Metadata } from "next";
-import Image from "next/image";
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join("content/projects"));
@@ -23,15 +22,15 @@ export async function generateMetadata({
   params: any;
 }): Promise<Metadata> {
   const project = getProject(params);
-
-  console.log("pro");
-  console.log(project.fontMatter);
   return {
     title: project.fontMatter.title,
     openGraph: {
       title: project.fontMatter.title,
       description: project.fontMatter.summary,
-      images: [project.fontMatter.imageShowcase || ""],
+      images:
+        project.fontMatter.imageShowcase !== undefined
+          ? [`/image-showcase/${project.fontMatter.imageShowcase}`]
+          : [""],
     },
   };
 }
@@ -56,11 +55,6 @@ export default function Page({ params }: any) {
   return (
     <article className="prose mb-10">
       <h1>{props.fontMatter.title}</h1>
-      {/* <Image
-        src={props.fontMatter.imageShowcase || ""}
-        width={500}
-        height={400}
-      /> */}
       <MDXRemote
         source={props.content}
         components={{ a: CustomLink, img: CustomImg }}
